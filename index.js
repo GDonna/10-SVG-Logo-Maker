a41d4936ff83f1ea2d02c22fdb98f42b480be01e
Donna Goon <g.donna120@gmail.com>
2023-10-28 08:55:17 -0500

    added shapes svg and fixed inquirer

--- index.js
+++ index.js
@@ -1,38 +1,5 @@
-const inquirer = require('inquirer');
-const fs = require('fs');
+const CLI = require('./lib/cli.js');
 
+const cli = new CLI();
 
-inquirer
-  .prompt([
-    {
-      type: 'input',
-      name: 'text',
-      message: 'Enter Three Characters',
-    },
-    {
-      type: 'input',
-      name: 'textcolor',
-      message: 'Enter Text Color',
-    },
-    {
-      type: 'input',
-      name: 'shape',
-      message: 'Choose a shape: Circle, Triangle, Square',
-    },
-    {
-      type: 'input',
-      name: 'shapecolor',
-      message: 'What is the color of the shape?',
-    },
-   
-  ])
-  .then((answers) => {
-    const htmlPageContent = generateHTML(answers);
-
-    fs.writeFile('index.html', htmlPageContent, (err) =>
-      err ? console.log(err) : console.log('Successfully created index.html!')
-    );
-  });
-
-//inquire prompts
-//create function to pull svg file in a string format function rendercircle 
+cli.run();
